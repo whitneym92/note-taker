342ec1b9cddd3fd6dd593599db014ec1a002d038
Whitney Monk <whitneym92@gmail.com>
2020-09-09 13:06:46 -0500

    updated api routes with get, post, and delete

---
+++ routes/apiRoutes.js
@@ -0,0 +1,33 @@
+const db = require("../db/db.json");
+const fs = require("fs");
+
+//routes 
+module.exports = function(app) {
+    //get request
+    app.get("/api/notes", function(req, res) {
+        res.json(db);
+    });
+
+    //post request
+    app.post("/api/notes", function(req, res) {
+        db.push(req.body);
+        db.forEach((obj, j) => {
+            obj.id = j + 1;
+        })
+        fs.writeFile(db, JSON.stringify(db), () => {
+            res.json(db);
+        })
+    });
+
+    //delete request
+    app.delete("/api/notes/:id", function(req,res) {
+        let id = req.params.id;
+        db.splice(id -1, 1);
+        db.forEach((obj, j) => {
+            obj.id = j + 1;
+        });
+        fs.writeFile(db, JSON.stringify(db), () => {
+            res.json(db);
+        });
+    });
+};
