a44d05edd0eb47559db02d762df4d02da7105c88
Whitney Monk <whitneym92@gmail.com>
2020-09-09 11:54:06 -0500

    set up html routes with get requests

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,13 @@
+//including path package to get the file path to our html
+let path = require("path");
+
+//routing HTML get requests
+module.exports = function(app) {
+    app.get("/notes", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/notes.html"));
+    });
+
+    app.get("/index", function(req,res) {
+        res.sendFile(path.join(__dirname, "../public/index.html"));
+    });
+}
